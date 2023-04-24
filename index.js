731d1010eeac788c223cf589e0cb78af9b9a5c86
Javier Valencia <valenciavazquez5@gmail.com>
2023-04-23 21:25:38 -0600

    Acualizacion 8

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const express = require('express');
 const app = express();
@@ -16,13 +15,17 @@ OPERADORES TERNARIOS
 */ 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 app.get("/", (req,res,next)=>{
-    return res.status(200).send("Bienvenido al Pokedex");
+    return res.status(200).json({code: 1, message: "Bienvedido al Pokedex"});
 });
 
 app.use("/pokemon", pokemon);
+
+app.use((req,res, next) => {
+    return res.status(404).json({code:404, message: "URL no encontrada"}) 
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('server is runing...')
